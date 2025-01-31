864f8b51899397727b4d533ea27ccd803dd5d5a9
BrenoSantos-aa <brenodasilvarocha34@gmail.com>
2025-01-31 09:31:11 -0300

    Update BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,5 +1,4 @@
-      pcall(function()
-       repeat
+      pcall(function(repeat)
 local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("ChooseTeam", true)
     local UIController = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("UIController", true)
     if UIController and ChooseTeam then
@@ -10447,6 +10446,4 @@ function FastAttackConnectorFunction()
     return ReturnFunctions
 end
 return FastAttackConnectorFunction()
-wait(1)
-       until condição
-   end)
+end
