e8f5269c15ea5050b06e1243d594197696e676ee
BrenoSantos-aa <brenodasilvarocha34@gmail.com>
2025-01-31 17:41:21 -0300

    Update BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,4 +1,4 @@
-      pcall(function() return repeat
+      pcall(function()repeat
 local ChooseTeam = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("ChooseTeam", true)
     local UIController = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("UIController", true)
     if UIController and ChooseTeam then
